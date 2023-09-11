97eedfeeb6be2ea5ce6c4eb64e53afd2632dcc8d
Kuba Solarz <solarzjakub97@gmail.com>
2023-09-11 22:52:16 +0200

    remove basename

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { MainPage, AccountantsPage } from './pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App: FC = () => (
-  <Router basename={'/infakt'}>
+  <Router>
     <Routes>
       <Route path={'/'} element={<MainPage />} />
       <Route path={'/ksiegowi'} element={<AccountantsPage />} />
